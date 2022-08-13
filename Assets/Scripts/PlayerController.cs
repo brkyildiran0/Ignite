1766271fd24b0e6f33209e56a0c71637d1c8c71d
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-08-13 03:10:00 +0300

    Animation Switch Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,27 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 10.0f;
+
     Rigidbody2D body;
 
     float horizontal;
     float vertical;
 
-    public float runSpeed = 10.0f;
+    Animator animator;
 
-    void Start()
+    void Awake()
     {
+        animator = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         GetInput();
+        HandleAnimation();
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
+    {
+        HandleVelocity();
+    }
+
+    private void HandleAnimation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.Play("PlayerLeft");
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            animator.Play("PlayerBack");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            animator.Play("PlayerFront");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.Play("PlayerRight");
+        }
+    }
+
+    private void HandleVelocity()
     {
         body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
