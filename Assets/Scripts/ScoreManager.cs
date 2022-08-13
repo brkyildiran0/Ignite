6a3e651665ed8e3a293a4bcf5e5248c79c0c9428
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-08-13 14:42:06 +0300

    High Score Implemented

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    public TMP_Text hiScoreText;
+
+    private int score = 0;
+    private int hiScore = 0;
+
+    private float scoreDivider = 0;
+
+    void Start()
+    {
+        hiScore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = score.ToString();
+        hiScoreText.text = hiScore.ToString();
+    }
+
+    void Update()
+    {
+        scoreDivider += Time.deltaTime;
+
+        if (scoreDivider > 0.1f)
+        {
+            score++;
+            scoreText.text = score.ToString();
+            scoreDivider = 0;
+        }
+
+        if (hiScore < score)
+        {
+            PlayerPrefs.SetInt("highscore", score);
+        }
+    }
+}
