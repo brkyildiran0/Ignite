9268eedbb2c87dd0c7309f7935a342ee567530da
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-08-14 16:43:43 +0300

    Asset Import (Check Desc)
    -Swords added, scaled accordingly.
    -Enemy Death VFX added, scaled accordingly.
    -Explosion VFX added, scaled accordingly.

---
+++ Assets/Scripts/GameObserveManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObserveManager : MonoBehaviour
+{
+    //2. Show Ignite? prompt
+    //3. Upon Input, 
+
+    //2. Enable:
+    //  2.1. POOL
+    //  2.2. MANAGER - Spawn
+    //  2.3. LANTERNS
+    //  2.4. SWORD
+    //  2.5. 
+    
+}
