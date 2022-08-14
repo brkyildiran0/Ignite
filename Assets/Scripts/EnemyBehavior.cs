7d80b4981bcabdc8843c367cb86cb750a2568164
Burak Yıldıran <omerburakyildiran@gmail.com>
2022-08-14 03:44:38 +0300

    Cauldron Enemies Added

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -8,22 +8,85 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] Sprite enemySprite1;
     [SerializeField] Sprite enemySprite2;
     [SerializeField] Sprite enemySprite3;
+    [SerializeField] Sprite cauldronEnemySprite;
+    [SerializeField] int cauldronSpawnChancePercentage = 10;
+    [SerializeField] float cauldronEnemyKnockbackForce = 10f;
+    [SerializeField] float swordKnockbackForce = 10f;
 
     private Transform player;
     private SpriteRenderer spriteRenderer;
     private int spriteRandomizer = 0;
     private int mirrorRandomizer = 0;
+    private int cauldronRandomizer = 0;
+    private bool isCauldron = false;
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         spriteRandomizer = Random.Range(0, 3);
         mirrorRandomizer = Random.Range(0, 2);
+        cauldronRandomizer = Random.Range(0, 10);
     }
 
     private void OnEnable()
     {
         RandomizeSprite();
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.position, enemyMovementSpeed * Time.deltaTime);
+    }
+
+    IEnumerator RemoveRigidbodyWithDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        //Change enemy to normal
+        isCauldron = false;
+
+        //Remove rigidbody
+        Destroy(gameObject.GetComponent<Rigidbody2D>());
+        GetComponent<BoxCollider2D>().isTrigger = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Weapon" && isCauldron)
+        {
+            //Physics
+            GetComponent<Rigidbody2D>().AddForceAtPosition(GetComponent<Rigidbody2D>().velocity * cauldronEnemyKnockbackForce, collision.transform.position);
+            Rigidbody2D swordRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            swordRb.AddForceAtPosition(-swordRb.velocity * swordKnockbackForce, collision.transform.position);
+
+            switch (spriteRandomizer)
+            {
+                case 0:
+                    spriteRenderer.sprite = enemySprite1;
+                    break;
+                case 1:
+                    spriteRenderer.sprite = enemySprite2;
+                    break;
+                case 2:
+                    spriteRenderer.sprite = enemySprite3;
+                    break;
+            }
+
+            StartCoroutine(RemoveRigidbodyWithDelay());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Weapon" && !isCauldron)
+        {
+            GetComponent<PooledObject>().Finish();
+        }
+
+        if (collision.tag == "Player")
+        {
+            //TODO, lose HP
+        }
     }
 
     private void RandomizeSprite()
@@ -43,6 +106,18 @@ public class EnemyBehavior : MonoBehaviour
                 break;
         }
 
+        //Chance for the enemies to be shielded
+        if (cauldronRandomizer == 0)
+        {
+            spriteRenderer.sprite = cauldronEnemySprite;
+            isCauldron = true;
+
+            //Add rigidbody to cauldron enemies to activate knockback effects
+            gameObject.AddComponent<Rigidbody2D>();
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+            GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+
         switch (mirrorRandomizer)
         {
             case 0:
@@ -51,24 +126,6 @@ public class EnemyBehavior : MonoBehaviour
             case 1:
                 spriteRenderer.flipX = false;
                 break;
-        }
-    }
-
-    void Update()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, player.position, enemyMovementSpeed * Time.deltaTime);
-    }
-
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Weapon")
-        {
-            GetComponent<PooledObject>().Finish();
-        }
-
-        if (collision.tag == "Player")
-        {
-            //TODO, game over
         }
     }
 }
